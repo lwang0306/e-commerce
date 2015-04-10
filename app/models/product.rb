e42b5ef509c752b5b36d327777d3349e6b388a11
elsapeng <elsa.peng@hotmail.com>
2015-04-09 21:20:37 -0400

    add validations for product and blog and category

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,7 @@
 class Product < ActiveRecord::Base
   has_and_belongs_to_many :categories
+
+  validates :name, presence: true
+  validates :price, presence: true, numericality: {greater_than_or_equal_to: 0}
+  validates :description, length: {maximum: 2000, too_long: "%{count} characters is the maximum allowed"}
 end
