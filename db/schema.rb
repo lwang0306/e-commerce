1522c99f276143fc79bdfbc372326df70bcb3f05
elsapeng <elsa.peng@hotmail.com>
2015-04-16 21:51:29 -0400

    add welcome page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150413195313) do
+ActiveRecord::Schema.define(version: 20150417003205) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
@@ -64,5 +64,10 @@ ActiveRecord::Schema.define(version: 20150413195313) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "welcomes", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
