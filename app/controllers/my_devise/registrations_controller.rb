066692c842a809fb29852982d0838e0c8b85e8ec
elsapeng <elsa.peng@hotmail.com>
2015-04-18 18:22:31 -0400

    add files

---
+++ app/controllers/my_devise/registrations_controller.rb
@@ -0,0 +1,35 @@
+class MyDevise::RegistrationsController < Devise::RegistrationsController
+
+  # GET /resource/sign_up
+  def new
+    super
+  end
+
+  # POST /resource
+  def create
+    # super
+
+    render 'welcomes/admin_page'
+  end
+
+  # GET /resource/edit
+  def edit
+    super
+  end
+
+  # PUT /resource
+  def update
+    super
+  end
+
+  # DELETE /resource
+  def destroy
+    super
+  end
+
+
+
+
+
+
+end
