9f63ef49bce3203d19f19f703a6bf2fc0d705bed
Luyao Wang <lwang0306@gmail.com>
2015-04-18 23:17:26 -0400

    implement delete method in product controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   has_and_belongs_to_many :categories, :through => :products_categories, :join_table => "products_categories"
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, :through => :users_products, :join_table => "users_products"
 
   validates :name, presence: true
   validates :price, presence: true, numericality: {greater_than_or_equal_to: 0}
