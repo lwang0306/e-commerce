cc04e5348dfb15049a4fd8817d8661be22e0cd8e
zhenbi <bizhen@seas.upenn.edu>
2015-04-19 00:54:25 -0400

    add shopping cart cucumber test

--- features/step_definitions/AddProductToShoppingCart.rb
+++ features/step_definitions/AddProductToShoppingCart.rb
@@ -22,13 +22,44 @@ When(/^I add a product to shopping cart$/) do
 end
 
 Then(/^I should be able to see the product in shopping cart$/) do
-  pending # express the regexp above with the code you wish you had
+  page.all('tr').each do |tr|
+    # next unless tr.has_selector?('Silk')
+
+    # Do stuff with trs that meet the href requirement
+    if (tr.all('td').size() > 3 and tr.all('td')[1].text == 'Silk') then
+      assert tr.all('td')[3].text == '1'
+      end
+  end
 end
 
 When(/^I add a product that is already in cart to shopping cart$/) do
-  pending # express the regexp above with the code you wish you had
+  visit(new_product_path)
+  fill_in 'Name', :with => 'Silk'
+  fill_in 'Price', :with => '3.90'
+  fill_in 'Description', :with => "Great product for your health!"
+  click_button 'Create Product'
+  visit(new_user_registration_path)
+  fill_in 'Name', :with => 'xiaoyuan'
+  fill_in 'Email', :with => 'bizhen@seas.upenn.edu'
+  fill_in 'Password', :with => '12345678'
+  fill_in 'Password confirmation', :with => '12345678'
+  fill_in 'Gender', :with => 'Female'
+  fill_in 'Age', :with => '12'
+  fill_in 'Phone number', :with => '1234567'
+  click_button 'Sign up'
+  visit(products_path)
+  click_link 'Add To Shopping Cart'
+  visit(products_path)
+  click_link 'Add To Shopping Cart'
 end
 
 Then(/^I should be able to see the product with updated quantity$/) do
-  pending # express the regexp above with the code you wish you had
+  page.all('tr').each do |tr|
+    # next unless tr.has_selector?('Silk')
+
+    # Do stuff with trs that meet the href requirement
+    if (tr.all('td').size() > 3 and tr.all('td')[1].text == 'Silk') then
+      assert tr.all('td')[3].text == '2'
+    end
+  end
 end
