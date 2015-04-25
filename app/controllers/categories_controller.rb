ffdffe810ce1d506b29e7ee31533c2697d3231fc
zhenbi <bizhen@seas.upenn.edu>
2015-04-24 21:50:35 -0400

    add view for admin homepage

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -13,17 +13,20 @@ class CategoriesController < ApplicationController
   # GET /categories/1.json
   def show
     @blogs = Blog.all.order('id desc')
+    render :layout => false
   end
 
   # GET /categories/new
   def new
     @category = Category.new
     @blogs = Blog.all.order('id desc')
+    render :layout => false
   end
 
   # GET /categories/1/edit
   def edit
     @blogs = Blog.all.order('id desc')
+    render :layout => false
   end
 
   # POST /categories
