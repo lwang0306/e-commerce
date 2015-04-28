cd4e7fb65a7ba40f583269c71eb2aa93143432ba
elsapeng <elsa.peng@hotmail.com>
2015-04-28 00:24:11 -0400

    bug fix on carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,5 +8,5 @@ CarrierWave.configure do |config|
       :aws_access_key_id      => 'AKIAJYCGOO66EVOPL5JA',                        # required
       :aws_secret_access_key  => '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'                         # required
   }
-  config.fog_directory  =  '597project'                   # required
+  config.fog_directory  =  '597pura'                   # required
 end
